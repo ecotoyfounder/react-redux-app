bd5188133e0d232f1f56c8c132cb15da1f98f8fa
Pavel <M3modelist@gmail.com>
2022-11-13 19:04:36 +0300

    adding files to react-redux-app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,50 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {initiateStore} from "./store/store";
+import * as actions from "./store/actions";
+
+const store = initiateStore()
+
+const App = () => {
+
+    const [state, setState] = useState(store.getState())
+
+    useEffect(() => {
+        store.subscribe(() => {
+            setState(store.getState())})
+    }, [])
+
+    const completeTask = (taskId) => {
+        store.dispatch(actions.taskCompleted(taskId))
+    }
+
+    const changeTitle = (taskId) => {
+        store.dispatch(actions.titleChanged(taskId))
+    }
+
+    return (
+        <>
+            <h1>App</h1>
+            <ul>{state.map(el => (
+                <li
+                    key={el.id}
+                >
+                    <p>{el.title}</p>
+                    <p>{`Completed: ${el.completed}`}</p>
+                    <button onClick={()=>completeTask(el.id)}>Complete</button>
+                    <button onClick={()=>changeTitle(el.id)}>Change Title</button>
+                    <hr/>
+                </li>
+            ))}
+            </ul>
+        </>
+    );
+};
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
